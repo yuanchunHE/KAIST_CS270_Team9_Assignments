22797c3b5b10321ab25e5ef2b854a4a5608f8518
yuanchunHE <102846967+yuanchunHE@users.noreply.github.com>
2022-04-02 16:38:06 +0900

    Add files via upload

---
+++ Assignment1/src/pack/Coordinate.java
@@ -0,0 +1,13 @@
+package pack;
+
+public class Coordinate {
+	String facingDire;
+	int x;
+	int y;
+	
+	Coordinate(){
+		facingDire="+x";
+		x=0;
+		y=0;
+	}
+}
---
+++ Assignment1/src/pack/GoForward.java
@@ -0,0 +1,32 @@
+package pack;
+
+import lejos.hardware.motor.Motor;
+import lejos.robotics.RegulatedMotor;
+
+public class GoForward extends Thread {
+	DataFlow dataFlow;
+
+	// leftMotor.synchronizeWith(new RegulatedMotor[] { rightMotor });
+
+	public GoForward(DataFlow df) {
+		dataFlow = df;
+	}
+
+	public void run() {
+		RegulatedMotor leftMotor = Motor.A;
+		RegulatedMotor rightMotor = Motor.B;
+
+		leftMotor.synchronizeWith(new RegulatedMotor[] { rightMotor });
+
+		while (true) {
+			if (dataFlow.getState() == 1) {
+				// go forward
+				leftMotor.startSynchronization();
+				leftMotor.backward();
+				rightMotor.backward();
+				leftMotor.endSynchronization();
+			}
+		}
+	}
+
+}
