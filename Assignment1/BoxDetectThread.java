d2593ece12e010fe31515598117815e2d89ee103
14Kgun <geon6757@kaist.ac.kr>
2022-04-03 20:52:29 +0900

    For detect thread test

---
+++ Assignment1/BoxDetectThread.java
@@ -0,0 +1,31 @@
+package pack;
+
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3IRSensor;
+import lejos.robotics.SampleProvider;
+
+public class BoxDetectThread extends Thread {
+	private final float maxDistance = 11.0;
+	private float distance = 0;
+	private EV3IRSensor distanceSensor;
+	
+	BoxDetectThread() {
+		distanceSensor = new EV3IRSensor(SensorPort.S2);
+	}
+
+	public void run() {
+		SampleProvider distanceMode = distanceSensor.getDistanceMode();
+		float value[] = new float[distanceMode.sampleSize()];
+		
+		int count = 0;// num of detected boxes
+		
+		while (true) {
+			distanceMode.fetchSample(value, 0);
+			distance = value[0]; // centimeter
+		}
+	}
+
+	public float getDistance() {
+		return distance;
+	}
+}
--- Assignment1/Robot.java
+++ Assignment1/Robot.java
@@ -9,78 +9,12 @@ import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.Color;
 
 public class Robot {
-	private static DataFlow dataFlow;
-	private static GoForward goForward;
-	private static BoxDetect boxDetect;
-	private static RedDetect redDetect;
-	private static Explorer explorer;
-	private static MoveAPI moveAPI;
-
 	public static void main(String[] args) {
-		EV3 ev3 = (EV3) BrickFinder.getLocal();
-		TextLCD lcd = ev3.getTextLCD();
-		Keys keys = ev3.getKeys();
+		BoxDetectThread boxDetectThread = new BoxDetectThread();
+		boxDetectThread.start();
 
-		// data start to flow
-		moveAPI = new MoveAPI();
-		dataFlow = new DataFlow();
-		// to get start pos and facingDir
-		dataFlow.setRobotPos(toGetStartPoint(dataFlow, 1));// 10->10s
-
-		boxDetect = new BoxDetect(dataFlow);
-		redDetect = new RedDetect(dataFlow);
-		goForward = new GoForward(dataFlow);
-		explorer = new Explorer(moveAPI, dataFlow);
-
-		boxDetect.start();
-		redDetect.start();
-		goForward.start();
-		explorer.start();
-		
-		while (keys.getButtons() != Keys.ID_ESCAPE) {
-
+		while (true) {
+			System.out.println(boxDetectThread.getDistance());
 		}
-		System.out.println("Finish!");
-        //System.out.print(explorer.getResult());
-		//lcd.drawString("Finish", 0, 0);
-	}
-
-	public static CoordinateSys toGetStartPoint(DataFlow df, int second) {
-		long startTime = System.currentTimeMillis();
-		long endTime = startTime + 1000 * second;
-
-		EV3ColorSensor leftColorSensor;
-		EV3ColorSensor rightColorSensor;
-
-		leftColorSensor = new EV3ColorSensor(SensorPort.S1);
-		rightColorSensor = new EV3ColorSensor(SensorPort.S3);
-
-		// System.out.println("toGetStartPoint Started!");
-		CoordinateSys startPos = new CoordinateSys();
-		// do {
-		// distinguish start point
-		if (leftColorSensor.getColorID() == Color.YELLOW
-				|| rightColorSensor.getColorID() == Color.YELLOW) {
-			// set (0,0) to the start point
-			startPos.x = 0;
-			startPos.y = 0;
-			startPos.facingDirection = 1;
-			// System.out.println("THIS IS (0,0).");
-			dataFlow.setState(1);
-		} else if (leftColorSensor.getColorID() == Color.BLUE
-				|| rightColorSensor.getColorID() == Color.BLUE) {
-			// set (5,3) to the start point
-			startPos.x = 5;
-			startPos.y = 3;
-			startPos.facingDirection = 0;
-			// System.out.println("THIS IS (5,3).");
-			dataFlow.setState(1);
-		}
-		// } while (System.currentTimeMillis() < endTime);
-
-		leftColorSensor.close();
-		rightColorSensor.close();
-
-		return startPos;
 	}
 }
