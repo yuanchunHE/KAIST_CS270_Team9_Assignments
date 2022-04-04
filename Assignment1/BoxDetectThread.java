4fd8f945d309b88fdfac6b33cdd67619b33d1a0a
14Kgun <geon6757@kaist.ac.kr>
2022-04-04 19:24:42 +0900

    Add Class SimulationApi

--- Assignment1/BoxDetectThread.java
+++ Assignment1/BoxDetectThread.java
@@ -5,24 +5,21 @@ import lejos.hardware.sensor.EV3IRSensor;
 import lejos.robotics.SampleProvider;
 
 public class BoxDetectThread extends Thread {
-	//private final float maxDistance = 11.0f;
 	private float distance = 0;
 	private EV3IRSensor distanceSensor;
 	private boolean flag;
 	
 	BoxDetectThread() {
-		flag=false;
+		flag = false;
 		distanceSensor = new EV3IRSensor(SensorPort.S2);
 	}
 
 	public void Stop(){
-		flag=true;
+		flag = true;
 	}
 	public void run() {
 		SampleProvider distanceMode = distanceSensor.getDistanceMode();
 		float value[] = new float[distanceMode.sampleSize()];
-		
-		//int count = 0;// num of detected boxes
 		
 		while (!flag) {
 			distanceMode.fetchSample(value, 0);
