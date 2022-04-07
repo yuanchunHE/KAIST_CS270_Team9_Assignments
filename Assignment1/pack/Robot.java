1ece41508be1f951a8647a1e5df160102f944c54
14Kgun <geon6757@kaist.ac.kr>
2022-04-07 19:30:34 +0900

    Delete return value of run function on algorithm

--- Assignment1/pack/Robot.java
+++ Assignment1/pack/Robot.java
@@ -5,11 +5,11 @@ import lejos.utility.Delay;
 public class Robot {
 	public static void main(String[] args) {
 		Api api = new Api();
-		//Explorer explorer = new Explorer(api);
+		Explorer explorer = new Explorer(api);
 		
-		//int[] startPos = api.getStartPos();
-		//while(!(explorer.run(startPos[0], startPos[1], startPos[2]))){};
-		api.goForward2();
+		int[] startPos = api.getStartPos();
+		explorer.run(startPos[0], startPos[1], startPos[2]);
+		//api.goForward2();
 		
     	System.out.println("Finish.");
     	//System.out.print(explorer.getResult());
