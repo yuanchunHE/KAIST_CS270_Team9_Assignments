5103e916b4df2368ad2cb89d35436ae107f8bc86
Dohyun Kim <65074958+dohyun1411@users.noreply.github.com>
2022-06-02 10:52:52 +0900

    Add ringTheBell

--- Project/ev3/Api.java
+++ Project/ev3/Api.java
@@ -4,8 +4,21 @@ public class Api {
     public Api() {
     }
 
-    public void ringTheBell() {
+    public boolean ringTheBell() {
         // ring the bell
+        // return true if robot succeeded to ring the bell, false otherwise
+        // i.e., false means human rang the bell first
+		RegulatedMotor motor = Motor.D;
+		motor.setSpeed((int) motor.getMaxSpeed());
+        
+        int i, max_i = 30;
+        for (i = 0; i < max_i; i++) {
+            Delay.msDelay(10);
+            if (touchDetectThread.isTouched()) {
+                return true;
+            }
+        }
+        return false;        
     }
 
     public void flipOneCard() {
