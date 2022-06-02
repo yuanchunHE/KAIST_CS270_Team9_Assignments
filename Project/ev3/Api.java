9d52bbec975679bd816e5c80d08e9e3d41a1c3ee
Dohyun Kim <65074958+dohyun1411@users.noreply.github.com>
2022-06-02 10:55:16 +0900

    Update Api.java

--- Project/ev3/Api.java
+++ Project/ev3/Api.java
@@ -8,8 +8,8 @@ public class Api {
         // ring the bell
         // return true if robot succeeded to ring the bell, false otherwise
         // i.e., false means human rang the bell first
-		RegulatedMotor motor = Motor.D;
-		motor.setSpeed((int) motor.getMaxSpeed());
+        RegulatedMotor motor = Motor.D;
+	motor.setSpeed((int) motor.getMaxSpeed());
         
         int i, max_i = 30;
         for (i = 0; i < max_i; i++) {
