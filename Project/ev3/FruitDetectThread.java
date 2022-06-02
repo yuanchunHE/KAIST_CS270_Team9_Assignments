33e83955a0005254f1cd12507320ba2f05266330
Dohyun Kim <65074958+dohyun1411@users.noreply.github.com>
2022-06-02 11:22:36 +0900

    Update FruitDetectThread.java

--- Project/ev3/FruitDetectThread.java
+++ Project/ev3/FruitDetectThread.java
@@ -2,6 +2,7 @@ package pack;
 
 public class FruitDetectThread extends Thread {
     FruitDetectThread() {
+	int status = 0;
     }
 
 	public void Stop() {
@@ -11,10 +12,30 @@ public class FruitDetectThread extends Thread {
     }
 
     public int getStatus() {
-        return 0;
+	status = 1234; // TODO: get status from socket
+        return status;
     }
 
     public boolean areThereFiveFruit() {
+	int tempStatus = status;
+	strawberry = tempStatus / 1000;
+	if (strawberry == 5) {
+		return true;
+	}
+	tempStatus -= strawberry * 1000;
+	banana = tempStatus / 100;
+	if (banana == 5) {
+		return true;
+	}
+	tempStatus -= banana * 100;
+	lime = tempStatus / 10;
+	if (lime == 5) {
+		return true;
+	}
+	plum = tempStatus - lime * 10;
+	if (plum == 5) {
+		return true;
+	}
         return false;
     }
 }
