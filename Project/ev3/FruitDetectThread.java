e08576b314071fbe5261278e3ad0b2eb108102ee
Dohyun Kim <65074958+dohyun1411@users.noreply.github.com>
2022-06-02 11:31:36 +0900

    string

--- Project/ev3/FruitDetectThread.java
+++ Project/ev3/FruitDetectThread.java
@@ -12,30 +12,17 @@ public class FruitDetectThread extends Thread {
     }
 
     public int getStatus() {
-	status = 1234; // TODO: get status from socket
         return status;
     }
 
     public boolean areThereFiveFruit() {
-	int tempStatus = status;
-	strawberry = tempStatus / 1000;
-	if (strawberry == 5) {
-		return true;
-	}
-	tempStatus -= strawberry * 1000;
-	banana = tempStatus / 100;
-	if (banana == 5) {
-		return true;
-	}
-	tempStatus -= banana * 100;
-	lime = tempStatus / 10;
-	if (lime == 5) {
-		return true;
-	}
-	plum = tempStatus - lime * 10;
-	if (plum == 5) {
-		return true;
-	}
-        return false;
+	String str = new String();
+	str = "1234"; // TODO: get status from socket
+	boolean success = Character.compare(str.charAT(0), '5') == 0) ||
+		Character.compare(str.charAT(1), '5') == 0) ||
+		Character.compare(str.charAT(2), '5') == 0) ||
+		Character.compare(str.charAT(3), '5') == 0)
+	    
+        return success;
     }
 }
